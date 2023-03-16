67a448df0e03ff10970ff6515995631c5121d8b2
joshualcaria <alcariajoshua13@gmail.com>
2023-03-16 15:41:41 +0800

    spotify

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,18 @@
 const toggleMenu = document.querySelector(".toggle__menu");
 const headerNav = document.querySelector(".header__nav");
+const blurr = document.querySelector('.arjay');
 toggleMenu.addEventListener("click", () => {
   toggleMenu.classList.toggle("open");
   headerNav.classList.toggle("open");
   headerNav.style=("transition: .5s ease")
+  blurr.classList.toggle("show");
 });
+
+const kamote = document.querySelectorAll('.title');
+kamote.forEach((kamoteq) => {
+    kamoteq.addEventListener('click', () => {
+        kamoteq.nextElementSibling.classList.toggle('open');
+        kamoteq.querySelector("i").classList.toggle("open");
+       
+    })
+})
